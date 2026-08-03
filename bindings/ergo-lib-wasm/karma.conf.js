9c289cc3cc07db629d06807612b768a1c668a292
kushti <kushti@protonmail.ch>
2026-08-04 00:03:11 +0300

    chrome fixes

--- bindings/ergo-lib-wasm/karma.conf.js
+++ bindings/ergo-lib-wasm/karma.conf.js
@@ -57,7 +57,19 @@ module.exports = function (config) {
     port: 9876,
     logLevel: config.LOG_INFO,
 
-    browsers: ["ChromeHeadless"],
+    browsers: ["ChromeHeadlessNoSandbox"],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+          "--headless=new",
+          "--disable-gpu",
+        ],
+      },
+    },
     browserNoActivityTimeout: 900000,
 
     autoWatch: false,
