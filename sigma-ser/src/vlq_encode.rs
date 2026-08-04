169211b9b59d12eb9b66ecb4fdb0d95699d194fa
kushti <kushti@protonmail.ch>
2026-08-04 23:56:35 +0300

    deps: alias core3 crate as core2 to avoid renaming imports across the workspace

--- sigma-ser/src/vlq_encode.rs
+++ sigma-ser/src/vlq_encode.rs
@@ -5,7 +5,7 @@ use alloc::{
     vec::Vec,
 };
 use core::convert::TryFrom;
-use core3::io;
+use core2::io;
 
 use bitvec::order::Lsb0;
 use bitvec::prelude::BitVec;
@@ -276,9 +276,9 @@ mod tests {
     use super::*;
     use alloc::format;
     use alloc::vec;
-    use core3::io::Cursor;
-    use core3::io::Read;
-    use core3::io::Write;
+    use core2::io::Cursor;
+    use core2::io::Read;
+    use core2::io::Write;
     use proptest::collection;
 
     extern crate derive_more;
